4af9459508a1de398690a4dc5fdb5bd15a54703a
Liuchenling <wssgcg1213@qq.com>
2015-04-19 20:21:10 +0800

    增加eventproxy处理异步数据, 修复路由部分 理逻辑

--- src/js/index.js
+++ src/js/index.js
@@ -14,11 +14,12 @@ require.config({
     }
 });
 
-require(['slider', 'domReady!', 'mmState'], function() {
-    var dating = avalon.define({
-        $id: "dating",
-        title: "约吧",
-        sliderCb: function() {
+require(['eventproxy', 'slider', 'domReady!', 'mmState'], function(EventProxy) {
+    //debugger;
+    var ep; //用来装载EventProxy的实例对象
+    avalon.define({
+        $id: "main", //主vm
+        sliderCb: function() { //初始化slider
             var width = $(document).width(),
                 height = width * 0.5625;
             $('.slider').slider({
@@ -28,5 +29,71 @@ require(['slider', 'domReady!', 'mmState'], function() {
             });
         }
     });
-    avalon.scan();
+
+    avalon.state('home', {
+        controller: "main",
+        url: "/",
+        templateUrl: "tpl/indexCtrl.html",
+        onEnter: function(){
+            //define vms
+            if(!avalon.vmodels['nav']){
+                avalon.define({
+                    $id: "nav",
+                    title: "约"
+                });
+            }
+
+            if(!avalon.vmodels['slider']){
+                avalon.define({
+                    $id: "slider",
+                    items: [{}]
+                });
+            }
+
+            if(!avalon.vmodels['fliterBtns']){
+                avalon.define({
+                    $id: "fliterBtns"
+                });
+            }
+
+            console.log('onenter home');
+            //异步处理, getdata
+            ep = EventProxy.create('user', 'slider', 'fliter', function(user, slider, fliter) {
+                avalon.vmodels['slider']['items'] = slider;
+                console.log('before scan');
+                avalon.scan();
+            });
+
+            ep.emit('user');//todo user data
+            //$.post('') //todo 获取slider的数据
+            ep.emit('slider', [{
+                href: "#!/collect",
+                img: "imgs/test.png"
+            },{
+                href: "#!/collect",
+                img: "imgs/test.png"
+            }]);
+            ep.emit('fliter'); //todo 获取fliter的分类数据
+        }
+    });
+    avalon.state('collect', {
+        url: "/collect",
+        templateUrl: "tpl/collectCtrl.html",
+        onEnter: function(state) {
+            var nav = avalon.vmodels['nav'];
+            if(nav){
+                nav['title'] = "收藏";
+            }else{
+                avalon.define({
+                    $id: "nav",
+                    title: "收藏"
+                });
+            }
+        }
+    });
+    avalon.history.start({
+        basepath: "/"
+    });
+    avalon.router.navigate(avalon.history.fragment);
+    //avalon.scan();
 });
